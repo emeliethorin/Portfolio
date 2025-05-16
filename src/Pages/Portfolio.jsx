3997b08994e51fa1d90b0c63eb33041555fb4ae7
emeliethorin <emeliethorin10@gmail.com>
2025-05-16 12:26:52 +0200

    added button to my projects

--- src/Pages/Portfolio.jsx
+++ src/Pages/Portfolio.jsx
@@ -1,11 +1,26 @@
 import ProjectOne from "../Components/ProjectOne";
 import ProjectTwo from "../Components/ProjectTwo";
+import React, { useState } from 'react';
 
-function Portfolio () {
-    return <div className="flex-container">
-        <ProjectOne title="Group project" heading="About the project"/>
-        <ProjectTwo title="Animations" heading="About the assignment"/>
-        </div>
-}
+const LaunchProjects = () => {
+    const [showProjects, setShowProjects] = useState(false);
 
-export default Portfolio;
+    const handleClick = () => {
+        setShowProjects(true);
+    };
+
+    return (
+    <div>
+        <button className="submit-btn" onClick={handleClick}>Click to view projects</button>
+
+        {showProjects && ( 
+            <div className="flex-container">
+                <ProjectOne title="Group project" heading="About the project"/>
+                <ProjectTwo title="Animations" heading="About the assignment"/>
+            </div>
+        )}
+    </div>
+    );
+};
+
+export default LaunchProjects;
