a1cab1051c701d3956551b6cd29631ea779e23c7
emeliethorin <emeliethorin10@gmail.com>
2025-05-19 11:36:38 +0200

    handle onClick

--- src/Components/PopupWindow.jsx
+++ src/Components/PopupWindow.jsx
@@ -3,17 +3,17 @@ import "../popupwindow.css";
 
 const PopupWindow = ({ title, info, onClick }) => {
     console.log("PopupWindow rendered");
-    
+
     const stopPropagation = (e) => {
         e.stopPropagation();
     };
 
     return (
-        <div className="popup-overlay">
-            <div className="popup-window">
+        <div className="popup-overlay" onClick={onclick}>
+            <div className="popup-window" onClick={stopPropagation}>
                 <h2>{title}</h2>
                 <p>{info}</p>
-                <a href="https://github.com/ssannejohansson/SpaceWorld" className="project-link">Github repo</a>
+                <a href="https://github.com/ssannejohansson/SpaceWorld" className="project-link" onClick={e => e.stopPropagation()}>Github repo</a>
                 <button onClick={onClick} className="close-button">
                     Close</button>
                     
